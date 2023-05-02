e012646ed3d75eeb8c5a3b56a4851e3c3112f9e2
Caroline Vézina <caroline_vezina@hotmail.com>
2023-05-02 16:18:54 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react'
+import Banner from './Banner'
+import logo from '../assets/logo.png'
+import Recommendation from './Recommendation'
+import Cart from './Cart'
+import ShoppingList from './ShoppingList'
+import Footer from './Footer'
+import '../styles/Layout.css'
+
+function App() {
+	const savedCart = localStorage.getItem('cart')
+	const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : [])
+	useEffect(() => {
+		localStorage.setItem('cart', JSON.stringify(cart))
+	}, [cart])
+
+	return (
+		<div>
+			<Banner>
+        <div className='lmj-title-logo'>
+          <img src={logo} alt='La maison jungle' className='lmj-logo' />
+				  <h1 className='lmj-title'>La maison jungle</h1>
+        </div>				
+        <Recommendation />
+			</Banner>
+			<div className='lmj-layout-inner'>
+				<Cart cart={cart} updateCart={updateCart} />
+				<ShoppingList cart={cart} updateCart={updateCart} />
+			</div>
+      <Footer />
+		</div>
+	)
+}
+
+export default App
